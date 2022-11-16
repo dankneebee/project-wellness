71f197707caae33213f1c65e6f5378868b1c9345
Dani <95629687+dankneebee@users.noreply.github.com>
2022-11-16 14:28:11 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express'),
+      app = express(),
+      server = require('http').createServer(app),
+      io = require('socket.io')(server)
+
+const CANVAS_ROWS = 500
+const CANVAS_COLS = 500
+
+var canvas = [ ]
+
+for(var row = 0; row < CANVAS_ROWS; row++){
+  canvas[row] = [ ]
+  
+  for(var col = 0; col < CANVAS_COLS; col++){
+    canvas[row][col] = "#FFF"
+  }
+}
+
+app.use(express.static("public"))
+
+io.on("connection", socket => {
+  socket.emit("canvas", canvas)
+
+  socket.on("color", data => {
+    if(data.row <= CANVAS_ROWS && data.row > 0 && data.col <= CANVAS_COLS && data.col > 0){
+ 
+      canvas[data.row - 1][data.col - 1] = data.color
+
+      io.emit("canvas", canvas)
+    }
+  })
+})
+
+
+server.listen(process.env.PORT || 3000)
